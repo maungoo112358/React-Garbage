d71c312299612101fdedc8805c08c7411dfb9056
Slint112358 <maungmaungoo.venuslab@gmail.com>
2022-07-07 11:25:35 +0630

    second commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">BOOTS</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">BAGS</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">GLOVES</h1>
+            <span className="title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
